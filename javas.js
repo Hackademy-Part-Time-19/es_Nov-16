1e1841524bfe5df8fce6064097d2406ae5a5f92e
Vincenzo Tito <vincenzotito@icloud.com>
2023-11-17 17:58:17 +0100

    Ultimo Salvataggio

--- javas.js
+++ javas.js
@@ -21,53 +21,7 @@ for (let i = 0; i < array.length; i++){
 
 //esercizio n.3
 
-/*let contoCorrente = 3000;
-let operazione;
 
-while (operazione != 4) {
-  operazione = parseInt(prompt("Benvenuto, selezioni l'opzione desiderata.\n\n Premi 1 per fare un bonifico\n\nPremi 2 per fare un prelievo\n\nPremi 3 per visualizzare il saldo\n\nPremi 0 per ritirare la carta"));
-
-  switch (operazione) {
-    case 1:
-      importoSelezionato = parseInt(prompt("Inserisci l'importo dell'operazione"));
-
-      if (contoCorrente - importoSelezionato > 0) {
-        let destinatario = parseInt(prompt("Inserisci l'Iban del destinatario:"));
-        contoCorrente = contoCorrente - importoSelezionato;
-
-        alert("Il Bonifico destinato a " + destinatario + "è andato a buon fine");
-        alert("Il saldo agggiornato è di " + contoCorrente + euro);
-      }
-
-      break;
-
-    case 2:
-      importoSelezionato = parseInt(prompt("Inserisci l'importo dell'operazione"));
-
-      if (contoCorrente - importoSelezionato > 0) {
-        contoCorrente = contoCorrente - importoSelezionato;
-
-        alert("Il Prelievo di " + importoSelezionato + " è andata a buon fine");
-        alert("Il saldo agggiornato è di " + contoCorrente + euro);
-      } else {
-        alert("Siamo spiacenti, il Saldo non è sufficiente");
-      }
-      break;
-
-    case 3:
-      alert("Il suo saldo agggiornato è di " + contoCorrente + " euro");
-
-      break;
-
-    case 0:
-      alert("Grazie e torni a trovarci.");
-
-      break;
-
-    default:
-      alert("Errore. Opzione non supportata.");
-  }
-}*/
 
 //esercizio n.3
 
