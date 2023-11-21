f64bb904057a3ee07ffd20b2be69ce0283e50a73
Vincenzo Tito <vincenzotito@icloud.com>
2023-11-21 19:51:32 +0100

    salvataggio

--- es_Tito.js
+++ es_Tito.js
@@ -1,6 +1,5 @@
 // esercizio n.1
 
-
 let listaProdotti = [
     {
         id: 1,
@@ -30,5 +29,43 @@ let listaNome = listaProdotti.map(function (listaProdotti){
     return listaProdotti.nome
 })
 
-console.log(listaNome);
+
+let listaPrezzo = listaProdotti.map(function (listaProdotti){
+    return listaProdotti.prezzo
+})
+
+// console.log(listaNome);
+
+
+function listaFiltrata (listaProdotti){
+
+    let listaFiltrata = []
+
+for (let i = 0; i < listaProdotti.length; i++) {
+    
+    if(listaProdotti[i].prezzo > 250){
+
+        listaFiltrata.push(listaProdotti[i])
+
+    }
+    
+}
+
+    return listaFiltrata;
+}
+
+let listaPrezzoMaggiore250 = listaFiltrata(listaProdotti);
+
+
+// console.log(listaPrezzoMaggiore250);
+
+
+
+
+let listaPrezzoMaggiore250_2 = listaProdotti.filter((listaPrezzo) => listaPrezzo.prezzo > 250)
+
+// console.log(listaPrezzoMaggiore250_2);
+
+
+// eswercizio n.2
 
